372e81eaf6b145c25e8a63129c9ec0a6343cbd89
Richard Smanhoto <richard.11smanhoto@gmail.com>
2023-11-30 19:21:36 -0400

    Criação da classe produtos junto com as funções de consulta e cadastro

--- classes/produtos.py
+++ classes/produtos.py
@@ -3,3 +3,17 @@ import sqlite3
 database = sqlite3.connect('./database/AutoPecas.db')
 cursor = database.cursor()
 
+class Produtos:
+    def __init__(self, nome: str, precoVenda: float, quantidade: int):
+        self.nome = nome
+        self.precovenda = precoVenda
+        self.quantidade = quantidade
+
+    def cadastroProduto(self):
+        cursor.execute(f"INSERT INTO dProdutos (nome, precoVenda, quantidade) VALUES ('{self.nome}', {self.precovenda}, {self.quantidade})")
+        database.commit()
+
+    def consultarProdutos(self):
+        cursor.execute(f"SELECT * FROM dProdutos")
+        print(cursor.fetchall())
+
